16140fcd728d0149722c63c5340ea48d1976772a
Raza_Web Development <razabtech27@gmail.com>
2025-10-30 19:54:04 +0530

    user Input dynamically

--- OSLAB/lab_quest.sh
+++ OSLAB/lab_quest.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#fibonacci series
+#fibonacci series 1
 
-# T1=0
-# T2=1
+T1=0
+T2=1
 
-# read -p "Enter no. of fibonacci series required : " n
-# echo -n "$T1 $T2 "
-# for((i=3; i<=$n; i++))
-#  do
-#    Tnext=$((T1+T2))
-#    echo -n "$Tnext "
-#    T1=$T2
-#    T2=$Tnext
-# done
+read -p "Enter no. of fibonacci series required : " n
+echo -n "$T1 $T2 "
+for((i=3; i<=$n; i++))
+ do
+   Tnext=$((T1+T2))
+   echo -n "$Tnext "
+   T1=$T2
+   T2=$Tnext
+done
 
-# find greatest element in an array
+# find greatest element in an array 2
 
 # num=(5 6 12 15 35 56 2 29)
-
+#  echo "Enter the array's element(space-separated)"
+#  read -a num
 # max=-1
 # for n in "${num[@]}"
 #  do
@@ -27,28 +28,35 @@
 #    fi
 #  done
 
-#  echo "Greatest element of array num [ ${num[@]} ] = $max"
+#  echo "Greatest element of the array num [ ${num[@]} ] = $max"
 
- # unique element if frequency of array's element is even
+ # unique element if frequency of array's element is even 3
+
+#  num=(4 4 2 2 1 5 2 3 4 2 5 4 1 5 6 5 3)
+#  echo "Enter the array's element(space-separated)"
+#  read -a num
 
 #  declare -A freq # associative array for frequency count
-#  num=(4 4 2 2 1 5 2 3 4 2 5 4 1 5 6 5 3)
 
 #  uni=0
 #  for n in "${num[@]}"
 #   do
 #     # uni=$((uni^n))
 #     ((freq[$n]++))
-  # done
-# echo "Unique element in the array num [ ${num[@]} ] = $uni"
-# echo "Frequency of each element of array num [ ${num[@]} ] is "
+#   done
+# #  echo "Unique element in the array num [ ${num[@]} ] = $uni"
+# # echo "Frequency of each element of array num [ ${num[@]} ] is "
 
-# for key in "${!freq[@]}"
+# for key in "${!freq[@]}" 
 #  do 
-#   echo "$key appears ${freq[$key]} times"
+#   # echo "$key appears-----> ${freq[$key]} times"
+#    if (( "${freq[$key]}" == 1)); then
+#       echo "The element $key is an unique element in array num [ ${num[*]} ]"
+#       exit
+#    fi
 #  done
 
-# factorial unsing function
+# factorial unsing function 5
 
 # fact() {
 #   n=1
@@ -64,9 +72,11 @@
 # echo "$x! = $(fact $x)"
 
 
-# sort the given array 
+# sort the given array  6
 
 # arr=(4 2 13 6 15 7 2 9)
+# echo "Enter array's element(space-separeted)"
+# read -a arr
 # echo "Before sorting the array arr = [ ${arr[*]} ]"
 # n="${#arr[@]}"
 
@@ -83,7 +93,7 @@
 #   done
 # echo "After sorting the array arr = [ ${arr[@]} ]"
 
-# reverse and pallindrome
+# reverse and pallindrome 7
 
 # read -p "Enter a number : " n
 
@@ -104,18 +114,17 @@
 # fi
  
 
- # leap year on hold
+ # leap year on hold 8
 
-read -p "Enter a year : " yr
+# read -p "Enter a year : " yr
 
-if (( ($yr % 400 == 0) || ($yr%4 == 0) && ($yr%100 != 0)  )); then 
-  echo "$yr is a leap year"
-  else
-   echo "$yr is a normal year"
-fi
+# if (( ($yr % 400 == 0) || ($yr%4 == 0) && ($yr%100 != 0)  )); then 
+#   echo "$yr is a leap year"
+#   else
+#    echo "$yr is a normal year"
+# fi
 
-#prime number
-# read -p "Enter a num : " num 
+#prime number 9
 
 # isPrime() {
 #     local n=$1
@@ -127,7 +136,7 @@ fi
 #         echo 1
 #         return
 #     fi
-#    for((i=3; $i*$i<=n; i++))
+#    for((i=2; $i*$i<=n; i++))
 #     do
 #       if (( n % i ==0 )); then
 #         echo 0
@@ -136,6 +145,8 @@ fi
 #      done 
 #      echo 1
 # }
+
+# read -p "Enter a num : " num 
 
 # if (( $(isPrime $num) == 1)); then
 #   echo "$num is a Prime number"
